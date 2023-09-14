b31ebdfed7c9a34993e662a7fbad2129a354c26c
Asad Mehmood <asadpk80@gmail.com>
2023-09-14 03:19:36 -0400

    Restrictions on TextInputField & Little bit Design tweaks

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,8 +3,12 @@ import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInputField}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,7 +18,27 @@ function StartGameScreen() {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-  button: {
-    color: "black",
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInputField: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
+    marginVertical: 8,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    textAlign: "center",
   },
 });
