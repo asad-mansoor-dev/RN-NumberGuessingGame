379d46c9a828d6dec10a9400a074a092c7edc5ed
Asad Mehmood <asadpk80@gmail.com>
2023-09-18 02:58:13 -0400

    On Picking number navigate to Game Screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Alert, StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -24,7 +24,7 @@ function StartGameScreen() {
       );
       return;
     }
-    console.log("Valid Number");
+    onPickNumber(inputNumber);
   }
 
   return (
